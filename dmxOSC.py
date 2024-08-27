4e561e5ccd74d6469dbd9ad5b7ab1b0696d106b5
Miles Thorogood <miles.thorogood@gmail.com>
2024-08-27 15:19:01 -0700

    swqapped to enttec module

--- dmxOSC.py
+++ dmxOSC.py
@@ -3,24 +3,15 @@ import serial
 import time
 from pythonosc import dispatcher
 from pythonosc import osc_server
+from DMXEnttecPro import Controller
 
 # Setup the DMX Pro device
-def setup_dmx(device):
-    ser = serial.Serial(device, baudrate=250000)
-    ser.write([0])  # Reset DMX
-    return ser
+
 
 # Send DMX data to the Enttec DMX Pro
-def send_dmx(ser, channel, value):
-    data = [0] * 513  # DMX data for 512 channels + start byte
-    data[0] = 0x7E  # Start byte
-    data[1] = 6  # Send DMX data label
-    data[2] = len(data) & 0xFF  # LSB of data length
-    data[3] = (len(data) >> 8) & 0xFF  # MSB of data length
-    data[4] = 0  # Start code
-    data[channel + 4] = value  # Set the value for the specified channel
-    data[-1] = 0xE7  # End byte
-    ser.write(bytearray(data))
+def send_dmx(dmx, channel, value):
+    dmx.set_channel(channel, value)  # Sets DMX channel 1 to max 255
+    dmx.submit()  # Sends the update to the controller
 
 # Handler for OSC messages
 def osc_handler(unused_addr, args, ch, val):
@@ -29,15 +20,16 @@ def osc_handler(unused_addr, args, ch, val):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--ip", default="127.0.0.1", help="The ip to listen on")
+    parser.add_argument("--ip", default="192.168.2.249", help="The ip to listen on")
     parser.add_argument("--port", type=int, default=8000, help="The port to listen on")
     parser.add_argument("--device", default="/dev/ttyUSB0", help="The DMX device")
     args = parser.parse_args()
 
-    ser = setup_dmx(args.device)
+    #ser = setup_dmx(args.device)
+    dmx = Controller(args.device, auto_submit=True)
 
     dispatcher = dispatcher.Dispatcher()
-    dispatcher.map("/dmx", osc_handler, ser)
+    dispatcher.map("/dmx", osc_handler, dmx)
 
     server = osc_server.ThreadingOSCUDPServer((args.ip, args.port), dispatcher)
     print(f"Serving on {server.server_address}")
@@ -46,4 +38,4 @@ if __name__ == "__main__":
         server.serve_forever()
     except KeyboardInterrupt:
         print("Exiting")
-        ser.close()
+        dmx.close()
